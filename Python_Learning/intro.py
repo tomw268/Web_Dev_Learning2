6353b91213fa8284e4c31617979a8df8c6750a79
tomw268 <tomw268@gmail.com>
2021-10-31 11:07:13 -0700

    working on learning python data types and math functions

--- Python_Learning/intro.py
+++ Python_Learning/intro.py
@@ -1,1 +1,19 @@
-print(4 + 10)
+# Fundamental Data Types
+# integers
+# print(type(6))
+# print(type(2-4))
+# print(type(2*4))
+# print(type(2/4))
+
+
+# print(2**3)
+
+# print(5 // 4)
+
+# print(5%4)
+# print(6%4)
+
+# math functions
+
+print(round(3.1))
+print(abs(-20))
