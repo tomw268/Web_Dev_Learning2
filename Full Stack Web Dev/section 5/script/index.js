7e2df1ae46f9204e0ae31d550477266ccb20d01d
tomw268 <tomw268@gmail.com>
2022-06-22 12:20:15 -0700

    working with arrays

--- Full Stack Web Dev/section 5/script/index.js
+++ Full Stack Web Dev/section 5/script/index.js
@@ -1,2 +1,14 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Be Kind using Js';
+
+// Arrays
+// is a structue that allows you to store multi values in one single variable.
+// EXample
+let myData = [33, 'Tom', 'Portland'];
+myData[0] = 34;
+
+console.log(myData);
+
+myData[1] = 'Tom Walker';
+
+console.log(myData);
