62ab3e37a99b2d75bb73d5b8d3a62768e708e260
tomw268 <tomw268@gmail.com>
2022-10-11 07:06:16 -0700

    creating new lines with out printing sepratly

--- 100daysPython/day1/bandnamegen.py
+++ 100daysPython/day1/bandnamegen.py
@@ -4,3 +4,6 @@ print("Helllo World")
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+# create new lines
+print("Hello World!\nHelloWorld!")
